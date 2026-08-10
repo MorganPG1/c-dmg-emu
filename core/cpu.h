89ad9fe25942d2750c916a571af2b3bd6230b9ba
Morgan Slatter <morganpgcontact@gmail.com>
2026-08-10 11:38:42 +0100

    move cpu instructions from gb.h to cpu.h

---
+++ core/cpu.h
@@ -0,0 +1,70 @@
+/*
+ * C DMG Gameboy Emulator Project - MorganPG
+ * 
+ * core/cpu.h
+ * 
+ * Header file for the CPU instruction handler functions
+*/
+#ifndef CPU_H
+#define CPU_H
+
+#include "gb.h"
+// Instruction functions
+// Block 0
+void handle_ld_r16_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_r16mem_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_a_r16mem(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_imm16_sp(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_inc_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_dec_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_add_hl_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_inc_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_dec_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_r8_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_flags_etc(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_jr_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_jr_c_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_stop(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+
+// Block 1
+void handle_ld_r8_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_halt(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+
+// Block 2 
+void handle_alu_add(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_adc(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_sub(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_sbc(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_and(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_xor(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_or(dmg_gameboy_t *gb, uint8_t val);
+void handle_alu_cp(dmg_gameboy_t *gb, uint8_t val);
+
+void handle_alu_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+
+// Block 3
+void handle_alu_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ret_c(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ret(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_reti(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_jp_c_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_jp_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_jp_hl(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_call_c_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_call_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_rst(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_pop(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_push(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_prefix(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ldh_c_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ldh_imm8_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_imm16_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ldh_a_c(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ldh_a_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ldh_a_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_add_sp_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_hl_sp_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ld_sp_hl(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_di(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+void handle_ei(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+#endif
--- core/gb.h
+++ core/gb.h
@@ -5,7 +5,12 @@
  * 
  * Header file for the main gameboy struct and functions
 */
+
+#ifndef GB_H
+#define GB_H
+
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef struct {
     union {
@@ -42,62 +47,6 @@ typedef struct {
 
     uint16_t pc;
     uint16_t sp;
+    bool running;
 } dmg_gameboy_t;
-
-// Block 0
-void handle_ld_r16_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_r16mem_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_a_r16mem(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_imm16_sp(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_inc_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_dec_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_add_hl_r16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_inc_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_dec_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_r8_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_flags_etc(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_jr_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_jrc_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_stop(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-
-// Block 1
-void handle_ld_r8_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_halt(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-
-// Block 2 
-void handle_alu_add(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_adc(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_sub(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_sbc(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_and(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_xor(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_or(dmg_gameboy_t *gb, uint8_t val);
-void handle_alu_cp(dmg_gameboy_t *gb, uint8_t val);
-
-void handle_alu_r8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-
-// Block 3
-void handle_alu_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ret_cond(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ret(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_reti(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_jp_cond_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_jp_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_jp_hl(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_call_cond_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_call_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_rst(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_pop(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_push(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_prefix(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ldh_c_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ldh_imm8_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_imm16_a(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ldh_a_c(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ldh_a_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ldh_a_imm16(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_add_sp_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_hl_sp_imm8(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ld_sp_hl(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_di(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
-void handle_ei(dmg_gameboy_t *gb, uint8_t opcode, uint8_t cycles[2]);
+#endif
