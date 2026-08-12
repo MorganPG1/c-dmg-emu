9cc4ff3caf4c4927a61b4c3f1091a1c9715aa960
Morgan Slatter <morganpgcontact@gmail.com>
2026-08-12 08:35:29 +0100

    implement timer (just a complete rewrite of the python one in c i havent changed much at all)

--- core/io.c
+++ core/io.c
@@ -1,5 +1,36 @@
+#include <stdbool.h>
 #include <stdint.h>
 #include "gb.h"
+static const uint8_t BIT_INDEXES[4] = {
+    9, // FREQ 0 - bit 9 high (1024 T-Cycles)
+    3, // FREQ 1 - bit 3 high (16 T-Cycles)
+    5, // FREQ 2 - bit 5 high (64 T-Cycles)
+    7  // FREQ 3 - bit 7 high (256 T-Cycles)
+};
+
+bool step_io(dmg_gameboy_t *gb, uint8_t cycles) {
+    bool irq = false;
+    uint8_t c;
+    for (c=0; c<cycles; c++) {
+        gb->master_counter++;
+        gb->div = (gb->master_counter >> 8) & 0xFF;
+        
+        bool en = (gb->tac >> 2) & 0b1;
+        uint8_t freq = (gb->tac) & 0b11;
+
+        uint8_t bit_ind = BIT_INDEXES[freq];
+        bool bit = (gb->master_counter >> bit_ind) & 0b1;
+
+        if ((!bit) && (gb->prev_signal) && (en)) {
+            gb->tima++;
+            if (gb->tima == 0) {
+                gb->tima = gb->tma;
+                irq = true;
+            }
+        }
+    }
+    return irq;
+}
 
 uint8_t read_io(dmg_gameboy_t *gb, uint16_t addr) {
     switch (addr) {
--- core/io.h
+++ core/io.h
@@ -1,8 +1,9 @@
 
 #include "gb.h"
+#include <stdbool.h>
 #include <stdint.h>
 
-void step_io(dmg_gameboy_t *gb, uint8_t cycles);
+bool step_io(dmg_gameboy_t *gb, uint8_t cycles);
 
 uint8_t read_io(dmg_gameboy_t *gb, uint16_t addr);
 void write_io(dmg_gameboy_t *gb, uint16_t addr, uint8_t val);
