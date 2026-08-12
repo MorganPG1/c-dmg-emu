875b4362cb41132d18486ee35e0d2624bd1542dd
Morgan Slatter <morganpgcontact@gmail.com>
2026-08-12 01:05:10 +0100

    update gitignore, print total cycles on exit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "core/cpu.h"
 #include "core/gb.h"
 #include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <time.h>
@@ -13,7 +14,7 @@
 #define TARGET_FRAME_TIME_NS 16742706
 
 static dmg_gameboy_t *global_gb = NULL;
-
+static uint64_t total_cycles = 0;
 uint64_t get_time_ns(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -31,18 +32,18 @@ void handle_sigint(int sig) {
     if (global_gb) {
         clean_exit(global_gb);
     }
+    printf("Total cycles: %lu\n", total_cycles);
     exit(0);
 }
 
 void mainloop(dmg_gameboy_t *gb) {
     uint64_t f_start = get_time_ns();
-
     while (gb->running) {
         uint32_t t_cycles = 0;
         while (t_cycles < CYCLES_PER_FRAME) {
             if (gb->debug) {
                 uint16_t pc = gb->pc;
-                GB_log("A:%02X F:%02X B:%02X C:%02X D:%02X E:%02X H:%02X L:%02X SP:%04X PC:%04X PCMEM:%02X,%02X,%02X,%02X IME:%i\n", gb->a, gb->f, gb->b, gb->c, gb->d, gb->e, gb->h, gb->l, gb->sp, pc, read_mem_8b(gb, pc), read_mem_8b(gb, pc+1), read_mem_8b(gb, pc+2),read_mem_8b(gb, pc+3), gb->ime);
+                GB_log("A:%02X F:%02X B:%02X C:%02X D:%02X E:%02X H:%02X L:%02X SP:%04X PC:%04X PCMEM:%02X,%02X,%02X,%02X IME:%i CYCLE:%i\n", gb->a, gb->f, gb->b, gb->c, gb->d, gb->e, gb->h, gb->l, gb->sp, pc, read_mem_8b(gb, pc), read_mem_8b(gb, pc+1), read_mem_8b(gb, pc+2),read_mem_8b(gb, pc+3), gb->ime, total_cycles);
             }
             gb->cycles = 0;
             if (!gb->halted) {
@@ -54,7 +55,7 @@ void mainloop(dmg_gameboy_t *gb) {
                 gb->cycles += 4;
             }
             t_cycles += gb->cycles;
-
+            total_cycles += gb->cycles;
             
             
         }
@@ -62,7 +63,7 @@ void mainloop(dmg_gameboy_t *gb) {
         uint64_t f_end = get_time_ns();
         uint64_t elapsed = f_end - f_start;
 
-        if (elapsed < TARGET_FRAME_TIME_NS) {
+        if (elapsed < TARGET_FRAME_TIME_NS && gb->debug) {
             struct timespec sleep_time;
             uint64_t remaining = TARGET_FRAME_TIME_NS - elapsed;
 
@@ -82,6 +83,7 @@ int main( int argc, char** argv ) {
 
     global_gb = gb;
     signal(SIGINT, handle_sigint);
+    signal(SIGTERM, handle_sigint);
 
     mainloop(gb);
     GB_log("Exiting, gb->running was set to false\n");
